004ee8b8699c89a41c54e40fd92d7303e112cdb4
Prettydamsel4 <alohmercy@gmail.com>
2022-09-09 15:30:05 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - Determines if a number is positive, negative or zero.
+ * main - prints a radom number and states whether                                                    * it is positive, negative or zero.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -13,17 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
+                                                                                                             if (n > 0)
+                printf("%d is positive\n", n);
+        else if (n > 0)
 		printf("%d is zero\n", n);
-	}							
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-return (0);
-}
+        else
+		printf("%d is negative\n", n); 
+                                                                                                             return (0);
+                                                                                                     }
