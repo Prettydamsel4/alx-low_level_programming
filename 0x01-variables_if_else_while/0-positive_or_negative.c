86f6c26e2c5c28b07a3f1a7a7d8cabf24e0b0ac3
Prettydamsel4 <alohmercy@gmail.com>
2022-09-11 05:16:47 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,32 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/* more headers goes there */
 
 /**
- * main - prints a radom number and states whether                                                    * it is positive, negative or zero.
- *
- * Return: Always 0.
- */
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+/* betty style doc for function main goes there */
 int main(void)
-                                                                                                     {
+{
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-                                                                                                             if (n > 0)
-                printf("%d is positive\n", n);
-        else if (n > 0)
-		printf("%d is zero\n", n);
-        else
-		printf("%d is negative\n", n); 
-                                                                                                             return (0);
-                                                                                                     }
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%i is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+	return (0);
+}
