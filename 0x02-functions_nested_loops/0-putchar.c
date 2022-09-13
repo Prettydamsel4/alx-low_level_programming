638c6b526990cbc747cae7f509e91b676ff1fad2
Prettydamsel4 <alohmercy@gmail.com>
2022-09-13 12:12:07 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,20 @@
 #include "main.h"
 
 /**
- * main - print putchar
- * Return: always 0
+ * main-Entry point
+ *
+ * Return: 0 if successful
  */
-
 int main(void)
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-
-        return (0);
-
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
-^C
