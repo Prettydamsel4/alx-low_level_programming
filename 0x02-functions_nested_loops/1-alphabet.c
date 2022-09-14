13404dfbeff56afaa5eb227fce8e010eab58ff0f
Prettydamsel4 <alohmercy@gmail.com>
2022-09-13 20:50:24 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_alphabet - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+        char c;
+        
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,25 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
+#include "main.h"
 
-int _putcher(char);
-void print_alphabet(void);
-void print-alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
-void num(int r, int n);
+/**
+ *
+ * *main - Entry point
+ *
+ * *
+ *
+ * *Return: 0 nothing, return void
+ *
+ * */                                                                                                                                                                                                                                                                                       int main(void)
 
-#endif
-^C
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
+        return (0);
+}
