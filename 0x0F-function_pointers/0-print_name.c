6a011166dc2f49a1c3d4793f3b32c37fb8f7b72d
Prettydamsel4 <alohmercy@gmail.com>
2022-10-12 06:24:13 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: string
- * @f: function
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-if (f != NULL)
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
