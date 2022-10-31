7ce4fc0156140b2f56ea37fc3e00460f01ff3d01
Prettydamsel4 <alohmercy@gmail.com>
2022-10-31 09:27:16 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,28 +2,33 @@
 
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: the name of the file
- * @text_content: the NULL terminated string to add at end of file
- * Return: 1 on success, -1 on failure
+ * @filename: the file to append to
+ * @text_content: the text that will be appended to the file
+ * Return: 1 for success & -1 failure
  */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int file, fwrite, i;
+	int f, r, l;
 
-    if (filename == NULL)
-        return (-1);
-    file = open(filename, O_APPEND | O_RDWR, 0664);
-    if (file == -1)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
+	f = open(filename, O_WRONLY | O_APPEND);
+	if (f == -1)
+		return (-1);
+	if (!text_content)
+		return (1);
+	l = 0;
+	while (text_content[l])
+		l += 1;
 
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i]; i++)
-            ;
-        fwrite = write(file, text_content, i);
-        if (fwrite == -1)
-            return (-1);
-    }
-    close(file);
-    return (1);
+	r = write(f, text_content, l);
+
+	if (r == -1)
+		return (-1);
+	r = close(f);
+	if (r == -1)
+		return (-1);
+
+	return (1);
 }
