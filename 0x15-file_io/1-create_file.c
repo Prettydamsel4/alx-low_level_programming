105564cc7b3adfee80110b94aa2441877f81edf8
Prettydamsel4 <alohmercy@gmail.com>
2022-10-31 09:25:40 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,28 +2,37 @@
 
 /**
  * create_file - creates a file
- * @filename: the name of the file to create
+ * @filename: filename to create
  * @text_content: a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure
- */
+ * Return: 1 if success) & -1 failure
+*/
+
 int create_file(const char *filename, char *text_content)
 {
-    int file, fwrite, i;
+	int f;
+	int l, r;
 
-    if (filename == NULL)
-        return (-1);
-    file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-    if (file == -1)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        for (i = 0; text_content[i]; i++)
-            ;
-        fwrite = write(file, text_content, i);
-        if (fwrite == -1)
-            return (-1);
-    }
-    close(file);
-    return (1);
+	/* open file*/
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (f == -1)
+		return (-1);
+
+	if (!text_content)
+		return (1);
+	l = 0;
+	while (text_content[l] != '\0')
+		l += 1;
+
+	r = write(f, text_content, l);
+	if (r != l)
+		return (-1);
+	r = close(f);
+	if (r == -1)
+		exit(-1);
+
+	return (1);
 }
