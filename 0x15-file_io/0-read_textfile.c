10d774ac844046638c1e1567240e51a2ba83539a
Prettydamsel4 <alohmercy@gmail.com>
2022-10-31 09:38:31 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,32 @@
 #include "main.h"
 
 /**
-* read_textfile - reads a text file and prints it to stdout
-* @filename: the file name to read from
-* @letters: the number of characters it should read and print
-* Return: the actual number of characters it could read and print
-* or 0 if error
-*/
-
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: pointer to text in a file
+ * @letters: number of letters
+ * Return: the actual number of letters it could read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int f, l, i;
-	int r;
-	char *buf;
+    ssize_t file, fread, fwrite;
+    char *totalSize;
 
-	if (filename == NULL)
-		return (0);
-	/* open file */
+    totalSize = malloc(sizeof(char) * letters);
+    if (totalSize == NULL)
+        return (0);
+    if (filename == NULL)
+        return (0);
 
-	f = open(filename, O_RDONLY);
-
-	if (f == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-
-	/* read in buf */
-	read(f, buf, letters);
-	buf[letters] = '\0';
-
-	for (i = 0; buf[i] != '\0'; i += 1)
-		l += 1;
-
-	/* close file*/
-	re = close(f);
-	if (r != 0)
-		exit(-1);
-	r = write(STDOUT_FILENO, buf, l);
-	if (r != l)
-		return (0);
-	free(buf);
-
-	return (l);
+    file = open(filename, O_RDONLY);
+    if (file == -1)
+        return (0);
+    fread = read(file, totalSize, letters);
+    if (fread == -1)
+        return (0);
+    fwrite = write(STDOUT_FILENO, totalSize, fread);
+    if (fwrite == -1)
+        return (0);
+    close(file);
+    free(totalSize);
+    return (fwrite);
 }
